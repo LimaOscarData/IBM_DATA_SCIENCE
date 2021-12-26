567c3774f2db32206f2aa5d409fc7c19a9f2f4d6
LimaOscarData <leventogut01@gmail.com>
2021-12-26 14:10:34 +0300

    line added added

--- WebScrapping_Review.py
+++ WebScrapping_Review.py
@@ -13,7 +13,7 @@ soup=BeautifulSoup(html, "html.parser")
 # print(soup.prettify())
 
 
-##Tags###################################
+######################## Tags ###################################
 tag_object=soup.title
 # print('tag object :', tag_object)
 # tag object : <title>Page Title</title>
@@ -27,7 +27,7 @@ tag_object=soup.h3
 # print(tag_object)
 # <h3><b id="boldest">Lebron James</b></h3>
 
-##Children, Parents, and Siblings###################################
+######################## Children, Parents, and Siblings ###################################
 # As stated above the Tag object is a tree of objects we can access the child of the tag or
 # navigate down the branch as follows:
 tag_child= tag_object.b
@@ -45,7 +45,8 @@ parent_tag= tag_child.parent
 # <body>
 # <h3><b id="boldest">Lebron James</b></h3><p> Salary: $ 92,000,000 </p>
 # <h3> Stephen Curry</h3><p> Salary: $85,000, 000 </p>
-# <h3> Kevin Durant </h3><p> Salary: $73,200, 000</p></body>
+# <h3> Kevin Durant </h3><p> Salary: $73,200, 000</p>
+# </body>
 
 # tag_object sibling is the paragraph element
 sibling_1= tag_object.next_sibling
@@ -63,7 +64,7 @@ sibling_3=sibling_2.next_sibling
 # print(sibling_3)
 # <p> Salary: $85,000, 000 </p>
 
-## HTML Attributes ###################################
+######################## HTML Attributes ###################################
 # If the tag has attributes, the tag id="boldest" has an attribute id whose value is boldest.
 # You can access a tag’s attributes by treating the tag like a dictionary:
 # print(tag_child['id'])
@@ -77,8 +78,8 @@ sibling_3=sibling_2.next_sibling
 # print(tag_child.get('id'))
 # boldest
 
-##Navigable String#################################
-tag_string=tag_child.string
+######################## Navigable String #################################
+tag_string=tag_child.string # tag_child : <b id="boldest">Lebron James</b>
 # print(tag_string)
 # Lebron James
 
@@ -89,15 +90,17 @@ unicode_string = str(tag_string)
 # print(unicode_string)
 # Lebron James
 
-# Filter#################################
-table="<table><tr><td id='flight'>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr><tr> <td>1</td><td><a " \
-      "href='https://en.wikipedia.org/wiki/Florida'>Florida<a></td><td>300 kg</td></tr><tr><td>2</td><td><a " \
-      "href='https://en.wikipedia.org/wiki/Texas'>Texas</a></td><td>94 kg</td></tr><tr><td>3</td><td><a " \
-      "href='https://en.wikipedia.org/wiki/Florida'>Florida<a> </td><td>80 kg</td></tr></table>"
+######################## Filter #################################
+table="<table>" \
+      "<tr><td id='flight'>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr>" \
+      "<tr> <td>1</td><td><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a></td><td>300 kg</td></tr>" \
+      "<tr><td>2</td><td><a href='https://en.wikipedia.org/wiki/Texas'>Texas</a></td><td>94 kg</td></tr>" \
+      "<tr><td>3</td><td><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a> </td><td>80 kg</td></tr>" \
+      "</table>"
 table_bs = BeautifulSoup(table, "html.parser")
 # print(table_bs)
 
-# find All#################################
+######################## find All #################################
 table_rows=table_bs.find_all('tr')
 # print(table_rows) #this is a list
 # [<tr><td id="flight">Flight No</td><td>Launch site</td> <td>Payload mass</td></tr>,
@@ -136,3 +139,89 @@ first_row =table_rows[0]
 #     cells=row.find_all('td')
 #     for j,cell in enumerate(cells):
 #         print('colunm',j,"cell",cell)
+
+# row 0
+# colunm 0 cell <td id="flight">Flight No</td>
+# colunm 1 cell <td>Launch site</td>
+# colunm 2 cell <td>Payload mass</td>
+# row 1
+# colunm 0 cell <td>1</td>
+# colunm 1 cell <td><a href="https://en.wikipedia.org/wiki/Florida">Florida<a></a></a></td>
+# colunm 2 cell <td>300 kg</td>
+# row 2
+# colunm 0 cell <td>2</td>
+# colunm 1 cell <td><a href="https://en.wikipedia.org/wiki/Texas">Texas</a></td>
+# colunm 2 cell <td>94 kg</td>
+# row 3
+# colunm 0 cell <td>3</td>
+# colunm 1 cell <td><a href="https://en.wikipedia.org/wiki/Florida">Florida<a> </a></a></td>
+# colunm 2 cell <td>80 kg</td>
+
+# If we use a list we can match against any item in that list.
+# print(table_bs)
+# list_input=table_bs.find_all(name=["tr", "td"])
+# print(list_input)
+
+######################## Attributes #################################
+# print(table_bs.find_all(id="flight"))
+
+# We can find all the elements that have links to the Florida Wikipedia page:
+list_input=table_bs.find_all(href="https://en.wikipedia.org/wiki/Florida")
+# print(list_input)
+# [<a href="https://en.wikipedia.org/wiki/Florida">Florida<a></a></a>,
+# <a href="https://en.wikipedia.org/wiki/Florida">Florida<a> </a></a>]
+
+# If we set the  href attribute to True, regardless of what the value is, the code finds all tags with href value:
+# print(table_bs.find_all(href=True))
+# [<a href="https://en.wikipedia.org/wiki/Florida">Florida<a></a></a>,
+#  <a href="https://en.wikipedia.org/wiki/Texas">Texas</a>,
+#  <a href="https://en.wikipedia.org/wiki/Florida">Florida<a> </a></a>]
+
+######################## Exercise: find_all ########################
+# Using the logic above, find all the elements without href value
+# print(table_bs.find_all(href=False))
+
+# Using the soup object soup, find the element with the id attribute content set to "boldest".
+# print(soup.find_all(id="boldest"))
+
+# string
+# With string you can search for strings instead of tags, where we find all the elments with Florida:
+# print(table_bs.find_all(string="Florida"))
+
+# find
+# The find_all() method scans the entire document looking for results, it’s if you are looking for one
+# element you can use the find() method to find the first element in the document. Consider the following two table:
+
+two_tables="<h3>Rocket Launch </h3>" \
+           "<p><table class='rocket'>" \
+           "<tr><td>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr>" \
+           "<tr><td>1</td><td>Florida</td><td>300 kg</td></tr>" \
+           "<tr><td>2</td><td>Texas</td><td>94 kg</td></tr>" \
+           "<tr><td>3</td><td>Florida </td><td>80 kg</td></tr>" \
+           "</table></p>" \
+           "<p><h3>Pizza Party  </h3><table class='pizza'>" \
+           "<tr><td>Pizza Place</td><td>Orders</td> <td>Slices </td></tr>" \
+           "<tr><td>Domino's Pizza</td><td>10</td><td>100</td></tr>" \
+           "<tr><td>Little Caesars</td><td>12</td><td >144 </td></tr>" \
+           "<tr><td>Papa John's </td><td>15 </td><td>165</td></tr>"
+# We create a BeautifulSoup object  two_tables_bs
+two_tables_bs= BeautifulSoup(two_tables, 'html.parser')
+
+# We can find the first table using the tag name table
+# print(two_tables_bs.find("table"))
+# <table
+# class="rocket"><tr><td>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr>
+# <tr><td>1</td><td>Florida</td><td>300 kg</td></tr>
+# <tr><td>2</td><td>Texas</td><td>94 kg</td></tr>
+# <tr><td>3</td><td>Florida </td><td>80 kg</td></tr>
+# </table>
+
+# We can filter on the class attribute to find the second table,
+# but because class is a keyword in Python, we add an underscore.
+print(two_tables_bs.find("table",class_='pizza'))
+# <table class="pizza">
+# <tr><td>Pizza Place</td><td>Orders</td> <td>Slices </td></tr>
+# <tr><td>Domino's Pizza</td><td>10</td><td>100</td></tr>
+# <tr><td>Little Caesars</td><td>12</td><td>144 </td></tr>
+# <tr><td>Papa John's </td><td>15 </td><td>165</td></tr>
+# </table>
