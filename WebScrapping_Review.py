4823b752d7e5e64e099732a0e541cb76deea77ad
LimaOscarData <leventogut01@gmail.com>
2021-12-27 13:01:06 +0300

    pandas and web scraping

--- WebScrapping_Review.py
+++ WebScrapping_Review.py
@@ -235,6 +235,90 @@ data  = requests.get(url).text
 # We create a BeautifulSoup object using the BeautifulSoup constructor
 soup = BeautifulSoup(data,"html.parser")  # create a soup object using the variable 'data'
 # Scrape all links
-for link in soup.find_all('a',href=True):  # in html anchor/link is represented by the tag <a>
-    print(link.get('href'))
+# for link in soup.find_all('a',href=True):  # in html anchor/link is represented by the tag <a>
+#     print(link.get('href'))
+
+################################# Scrape all images Tags #################################
+# for link in soup.find_all('img'): # in html image is represented by the tag <img>
+#     print(link)
+#     print(link.get('src'))
+
+################################# Scrape data from HTML tables #################################
+#The below url contains an html table with data about colors and color codes.
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-" \
+      "DA0321EN-SkillsNetwork/labs/datasets/HTMLColorCodes.html"
+# Before proceeding to scrape a web site, you need to examine the contents, and the way data is
+# organized on the website. Open the above url in your browser and check how many rows and columns
+# are there in the color table.
+
+# get the contents of the webpage in text format and store in a variable called data
+data  = requests.get(url).text
+
+soup = BeautifulSoup(data,"html.parser")
+
+#find a html table in the web page
+table = soup.find('table') # in html table is represented by the tag <table>
+
+#Get all rows from the table
+# for row in table.find_all('tr'): # in html table row is represented by the tag <tr>, find all makes a list
+#     # Get all columns in each row.
+#     cols = row.find_all('td') # in html a column is represented by the tag <td>, making a kind of list
+#     color_name = cols[2].string # store the value in column 3 as color_name
+#     color_code = cols[3].string # store the value in column 4 as color_code
+#     print("{}--->{}".format(color_name,color_code))
+
+############# Scrape data from HTML tables into a DataFrame using BeautifulSoup and Pandas ###############
+import pandas as pd
+#The below url contains html tables with data about world population.
+url = "https://en.wikipedia.org/wiki/World_population"
+
+# get the contents of the webpage in text format and store in a variable called data
+data  = requests.get(url).text
+
+soup = BeautifulSoup(data,"html.parser")
+
+#find all html tables in the web page
+tables = soup.find_all('table') # in html table is represented by the tag <table>
+
+# we can see how many tables were found by checking the length of the tables list
+# print(len(tables))
+
+# Assume that we are looking for the 10 most densly populated countries table, we can
+# look through the tables list and find the right one we are look for based on the data
+# in each table or we can search for the table name if it is in the table but this option
+# might not always work.
+
+for index,table in enumerate(tables):
+    if ("10 most densely populated countries" in str(table)):
+        table_index = index
+# print(table_index)
+
+# We can use the method prettify() to display the HTML in the nested structure
+# print(tables[table_index].prettify())
+
+population_data = pd.DataFrame(columns=["Rank", "Country", "Population", "Area", "Density"])
+
+for row in tables[table_index].tbody.find_all("tr"):
+    col = row.find_all("td")
+    if (col != []):
+        rank = col[0].text
+        country = col[1].text
+        population = col[2].text.strip()
+        area = col[3].text.strip()
+        density = col[4].text.strip()
+        population_data = population_data.append({"Rank":rank, "Country":country, "Population":population,
+                                                  "Area":area, "Density":density}, ignore_index=True)
+
+print(population_data)
+#   Rank           Country   Population     Area Density
+# 0    1         Singapore    5,704,000      710   8,033
+# 1    2        Bangladesh  171,930,000  143,998   1,194
+# 2    3  \n Palestine\n\n    5,266,785    6,020     847
+# 3    4           Lebanon    6,856,000   10,452     656
+# 4    5            Taiwan   23,604,000   36,193     652
+# 5    6       South Korea   51,781,000   99,538     520
+# 6    7            Rwanda   12,374,000   26,338     470
+# 7    8             Haiti   11,578,000   27,065     428
+# 8    9       Netherlands   17,670,000   41,526     426
+# 9   10            Israel    9,450,000   22,072     428
 
