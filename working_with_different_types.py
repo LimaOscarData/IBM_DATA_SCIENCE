ca07d3959a39cba62ef50dd7f05877ca44f50463
LimaOscarData <leventogut01@gmail.com>
2021-12-30 22:31:37 +0300

    added the final info

--- working_with_different_types.py
+++ working_with_different_types.py
@@ -332,6 +332,233 @@ import xml.etree.ElementTree as etree
 # Correspondingly, Pandas enables us to save the dataset to csv by using the dataframe.to_csv() method, you can
 # add the file path and name along with quotation marks in the parentheses.
 # For example, if you would save the dataframe df as employee.csv to your local machine, you may use the syntax below:
-datatframe.to_csv("employee.csv", index=False)
+# datatframe.to_csv("employee.csv", index=False)
 # We can also read and save other file formats, we can use similar functions to pd.read_csv() and df.to_csv() for
 # other data formats. The functions are listed in the following table:
+
+# Read/Save Other Data Formats
+# Data Formate	Read	Save
+# csv	pd.read_csv()	df.to_csv()
+# json	pd.read_json()	df.to_json()
+# excel	pd.read_excel()	df.to_excel()
+# hdf	pd.read_hdf()	df.to_hdf()
+# sql	pd.read_sql()	df.to_sql()
+# ...	...	...
+
+# Let's move ahead and perform some Data Analysis.
+
+# Binary File Format
+# "Binary" files are any files where the format isn't made up of readable characters. It contain formatting
+# information that only certain applications or processors can understand. While humans can read text files,
+# binary files must be run on the appropriate software or processor before humans can read them.
+# Binary files can range from image files like JPEGs or GIFs, audio files like MP3s or binary document formats
+# like Word or PDF.
+# Let's see how to read an Image file.
+
+# Reading the Image file
+# Python supports very powerful tools when it comes to image processing. Let’s see how to process the images
+# using the PIL library.
+# PIL is the Python Imaging Library which provides the python interpreter with image editing capabilities.
+
+# importing PIL
+# from PIL import Image
+# import urllib.request
+# # Downloading dataset
+# urllib.request.urlretrieve("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-"
+#                            "royalty-free-image-1586966191.jpg", "dog.jpg")
+#
+# # Read image
+# img = Image.open('dog.jpg')
+#
+# # Output Images
+# display(img)
+
+# Data Analysis
+# In this section, you will learn how to approach data acquisition in various ways and obtain necessary insights
+# from a dataset. By the end of this lab, you will successfully load the data into Jupyter Notebook and gain some
+# fundamental insights via the Pandas Library.
+# In our case, the Diabetes Dataset is an online source and it is in CSV (comma separated value) format. Let's use
+# this dataset as an example to practice data reading.
+
+# About this Dataset
+# Context: This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases.
+# The objective of the dataset is to diagnostically predict whether or not a patient has diabetes, based on certain
+# diagnostic measurements included in the dataset. Several constraints were placed on the selection of these
+# instances from a larger database. In particular, all patients here are females at least 21 years of age of Pima
+# Indian heritage.
+# Content: The datasets consists of several medical predictor variables and one target variable, Outcome. Predictor
+# variables includes the number of pregnancies the patient has had, their BMI, insulin level, age, and so on.
+# We have 768 rows and 9 columns. The first 8 columns represent the features and the last column represent the
+# target/label.
+
+# Import pandas library
+import pandas as pd
+path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-" \
+       "PY0101EN-SkillsNetwork/labs/Module%205/data/diabetes.csv"
+df = pd.read_csv(path)
+# After reading the dataset, we can use the dataframe.head(n) method to check the top n rows of the dataframe,
+# where n is an integer. Contrary to dataframe.head(n), dataframe.tail(n) will show you the bottom n
+# rows of the dataframe.
+
+# show the first 5 rows using dataframe.head() method
+# print("The first 5 rows of the dataframe")
+# print(df.head(5))
+# The first 5 rows of the dataframe
+#    Pregnancies  Glucose  BloodPressure  ...  DiabetesPedigreeFunction  Age  Outcome
+# 0            6      148             72  ...                     0.627   50        1
+# 1            1       85             66  ...                     0.351   31        0
+# 2            8      183             64  ...                     0.672   32        1
+# 3            1       89             66  ...                     0.167   21        0
+# 4            0      137             40  ...                     2.288   33        1
+#
+# [5 rows x 9 columns]
+
+# To view the dimensions of the dataframe, we use the .shape parameter.
+# print(df.shape)
+# (768, 9)
+
+####################### Statistical Overview of dataset #######################
+# print(df.info())
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 768 entries, 0 to 767
+# Data columns (total 9 columns):
+#  #   Column                    Non-Null Count  Dtype
+# ---  ------                    --------------  -----
+#  0   Pregnancies               768 non-null    int64
+#  1   Glucose                   768 non-null    int64
+#  2   BloodPressure             768 non-null    int64
+#  3   SkinThickness             768 non-null    int64
+#  4   Insulin                   768 non-null    int64
+#  5   BMI                       768 non-null    float64
+#  6   DiabetesPedigreeFunction  768 non-null    float64
+#  7   Age                       768 non-null    int64
+#  8   Outcome                   768 non-null    int64
+# dtypes: float64(2), int64(7)
+# memory usage: 54.1 KB
+# None
+
+# This method prints information about a DataFrame including the index dtype and columns, non-null values and
+# memory usage.
+
+
+# print(df.describe())
+#        Pregnancies     Glucose  ...         Age     Outcome
+# count   768.000000  768.000000  ...  768.000000  768.000000
+# mean      3.845052  120.894531  ...   33.240885    0.348958
+# std       3.369578   31.972618  ...   11.760232    0.476951
+# min       0.000000    0.000000  ...   21.000000    0.000000
+# 25%       1.000000   99.000000  ...   24.000000    0.000000
+# 50%       3.000000  117.000000  ...   29.000000    0.000000
+# 75%       6.000000  140.250000  ...   41.000000    1.000000
+# max      17.000000  199.000000  ...   81.000000    1.000000
+#
+# [8 rows x 9 columns]
+
+# Pandas describe() is used to view some basic statistical details like percentile, mean, standard deviation, etc.
+# of a data frame or a series of numeric values. When this method is applied to a series of strings, it returns a
+# different output
+
+
+######################## Identify and handle missing values ########################
+# We use Python's built-in functions to identify these missing values. There are two methods to detect missing data:
+# .isnull()
+# .notnull()
+# The output is a boolean value indicating whether the value that is passed into the argument is in fact missing data.
+missing_data = df.isnull()
+# print(missing_data.head(5))
+#    Pregnancies  Glucose  ...    Age  Outcome
+# 0        False    False  ...  False    False
+# 1        False    False  ...  False    False
+# 2        False    False  ...  False    False
+# 3        False    False  ...  False    False
+# 4        False    False  ...  False    False
+#
+# [5 rows x 9 columns]
+
+# "True" stands for missing value, while "False" stands for not missing value.
+
+
+# Count missing values in each column
+# Using a for loop in Python, we can quickly figure out the number of missing values in each column. As mentioned above,
+# "True" represents a missing value, "False" means the value is present in the dataset. In the body of the for loop
+# the method ".value_counts()" counts the number of "True" values.
+
+# for column in missing_data.columns.values.tolist():
+#     print(column)
+#     print (missing_data[column].value_counts())
+#     print("")
+
+# Pregnancies
+# False    768
+# Name: Pregnancies, dtype: int64
+#
+# Glucose
+# False    768
+# Name: Glucose, dtype: int64
+#
+# BloodPressure
+# False    768
+# Name: BloodPressure, dtype: int64
+#
+# SkinThickness
+# False    768
+# Name: SkinThickness, dtype: int64
+#
+# Insulin
+# False    768
+# Name: Insulin, dtype: int64
+#
+# BMI
+# False    768
+# Name: BMI, dtype: int64
+#
+# DiabetesPedigreeFunction
+# False    768
+# Name: DiabetesPedigreeFunction, dtype: int64
+#
+# Age
+# False    768
+# Name: Age, dtype: int64
+#
+# Outcome
+# False    768
+# Name: Outcome, dtype: int64
+
+# As you can see above, there is no missing values in the dataset.
+
+
+# Correct data format
+# Check all data is in the correct format (int, float, text or other).
+# In Pandas, we use
+# .dtype() to check the data type
+# .astype() to change the data type
+# Numerical variables should have type 'float' or 'int'.
+
+# print(df.dtypes)
+
+# Pregnancies                   int64
+# Glucose                       int64
+# BloodPressure                 int64
+# SkinThickness                 int64
+# Insulin                       int64
+# BMI                         float64
+# DiabetesPedigreeFunction    float64
+# Age                           int64
+# Outcome                       int64
+# dtype: object
+
+
+# Visualization
+# Visualization is one of the best way to get insights from the dataset. Seaborn and Matplotlib are two of Python's
+# most powerful visualization libraries.
+
+# import libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+labels= 'Diabetic','Not Diabetic'
+plt.pie(df['Outcome'].value_counts(),labels=labels,autopct='%0.02f%%')
+plt.legend()
+plt.show()
+
+# As you can see above, 65.10% females are Diabetic and 34.90% are Not Diabetic.
